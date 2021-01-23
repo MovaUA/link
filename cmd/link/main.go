08fd7d7db8c1543fd7a13a90826a063312ce130e
Valeriy <valeriy.molchanov.77@gmail.com>
2021-01-23 21:09:48 +0200

    link (#2)
    * link
    
    * cleanup
    
    * json output
    
    * refactoring: return []Link
    
    * refactoring: strings.Fields

--- cmd/link/main.go
+++ cmd/link/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	_ "github.com/movaua/link/pkg/link"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/movaua/link/pkg/link"
 )
 
 func main() {
+	filename := flag.String("file", "", "an HTML file name")
+	flag.Parse()
 
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	links, err := link.Find(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := json.NewEncoder(os.Stdout)
+	e.SetIndent("", "  ")
+	if err := e.Encode(links); err != nil {
+		log.Fatal(err)
+	}
 }
--- pkg/link/link.go
+++ pkg/link/link.go
@@ -1,1 +1,83 @@
 package link
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+// Link is a parsed representation of HTML <a> tag
+type Link struct {
+	Href string `json:"href,omitempty"`
+	Text string `json:"text,omitempty"`
+}
+
+// Find reads HTML from provided reader and returns found links
+func Find(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return findLinks(doc), nil
+}
+
+func findLinks(n *html.Node) []Link {
+	if isLink(n) {
+		link := buildLink(n)
+		return []Link{link}
+	}
+
+	var links []Link
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = append(links, findLinks(c)...)
+	}
+
+	return links
+}
+
+func isLink(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.DataAtom == atom.A
+}
+
+func buildLink(n *html.Node) Link {
+	var sb strings.Builder
+	text(n, &sb)
+	text := strings.Join(strings.Fields(sb.String()), " ")
+
+	return Link{
+		Href: href(n),
+		Text: text,
+	}
+}
+
+func href(n *html.Node) string {
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func text(n *html.Node, sb *strings.Builder) {
+	if isText(n) {
+		text := strings.TrimSpace(n.Data)
+		sb.WriteString(text)
+		if len(text) > 0 {
+			sb.WriteRune(' ')
+		}
+		return
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text(c, sb)
+	}
+}
+
+func isText(n *html.Node) bool {
+	return n.Type == html.TextNode
+}
