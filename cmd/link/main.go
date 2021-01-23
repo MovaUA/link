6c37367ce08f1d03c45fcf34713405fea57341d8
mova <valeriy.molchanov.77@gmail.com>
2021-01-23 19:18:15 +0200

    link

--- cmd/link/main.go
+++ cmd/link/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	_ "github.com/movaua/link/pkg/link"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/movaua/link/pkg/link"
 )
 
 func main() {
+	filename := flag.String("file", "", "an HTML file name")
+	flag.Parse()
 
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	links, err := link.Find(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", links)
 }
--- pkg/link/link.go
+++ pkg/link/link.go
@@ -1,1 +1,82 @@
 package link
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+// Link is a parsed representation of HTML <a> tag
+type Link struct {
+	Href string
+	Text string
+}
+
+// Find reads HTML from provided reader and returns found links
+func Find(r io.Reader) ([]Link, error) {
+	root, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []Link
+
+	for node := root; node != nil; node = node.NextSibling {
+		findLinks(node, &links)
+	}
+
+	return links, nil
+}
+
+func findLinks(n *html.Node, links *[]Link) {
+	if isLink(n) {
+		var sb strings.Builder
+		text(n, &sb)
+		link := Link{
+			Href: href(n),
+			Text: sb.String(),
+		}
+		if len(link.Text) > 0 {
+			link.Text = link.Text[:len(link.Text)-1]
+		}
+		*links = append(*links, link)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findLinks(c, links)
+	}
+}
+
+func isLink(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.DataAtom == atom.A
+}
+
+func href(n *html.Node) string {
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func text(n *html.Node, sb *strings.Builder) {
+	if isText(n) {
+		text := strings.TrimSpace(n.Data)
+		(*sb).WriteString(text)
+		if len(text) > 0 {
+			(*sb).WriteRune(' ')
+		}
+		return
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text(c, sb)
+	}
+}
+
+func isText(n *html.Node) bool {
+	return n.Type == html.TextNode
+}
